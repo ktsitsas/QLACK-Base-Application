6be82e0e8148fca9fd98cf02816f9b337b06931b
Kostis Tsitsas <37558185+ktsitsas@users.noreply.github.com>
2025-10-01 17:34:03 +0300

    fixed bug with PUBLIC_URIS by also adding them to ignore-paths of CustomCookieFilter

--- qlack-base-application-server/src/main/java/com/eurodyn/qlack/baseapplication/config/WebSecurityConfig.java
+++ qlack-base-application-server/src/main/java/com/eurodyn/qlack/baseapplication/config/WebSecurityConfig.java
@@ -1,6 +1,8 @@
 package com.eurodyn.qlack.baseapplication.config;
 
+import com.eurodyn.qlack.util.csrf.filter.CustomCookieFilter;
 import com.eurodyn.qlack.util.jwt.filter.JwtAuthenticationFilter;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -9,33 +11,33 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
-import com.eurodyn.qlack.util.csrf.filter.CustomCookieFilter;
 
 @Configuration
 public class WebSecurityConfig {
 
-  private static final String[] PUBLIC_URIS =
-    {"/users/auth", "/ping", "/i18n/*","/error"};
-  private final JwtAuthenticationFilter jwtAuthenticationFilter;
+    @Value("${qlack.util.csrf.ignore-paths:#{null}}")
+    private List<String> IGNORED_PATHS;
+
+    private final JwtAuthenticationFilter jwtAuthenticationFilter;
 
     private final CustomCookieFilter customCookieFilter;
 
-  public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomCookieFilter customCsrfCookieFilter) {
-    this.jwtAuthenticationFilter = jwtAuthenticationFilter;
-    this.customCookieFilter = customCsrfCookieFilter;
-  }
+    public WebSecurityConfig(JwtAuthenticationFilter jwtAuthenticationFilter, CustomCookieFilter customCsrfCookieFilter) {
+        this.jwtAuthenticationFilter = jwtAuthenticationFilter;
+        this.customCookieFilter = customCsrfCookieFilter;
+    }
 
-  @Bean
-  public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-    http.csrf(AbstractHttpConfigurer::disable)
-        .authorizeHttpRequests(requests -> requests
-            .requestMatchers(PUBLIC_URIS).permitAll()
-            .anyRequest().authenticated())
-        .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-        .addFilterBefore(customCookieFilter, BasicAuthenticationFilter.class)
-        .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        final String[] PUBLIC_URIS = IGNORED_PATHS!=null ? IGNORED_PATHS.toArray(new String[IGNORED_PATHS.size()]) : new String[0];
+        http.csrf(AbstractHttpConfigurer::disable)
+            .authorizeHttpRequests(requests -> requests
+                .requestMatchers(PUBLIC_URIS).permitAll()
+                .anyRequest().authenticated())
+            .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+            .addFilterBefore(customCookieFilter, BasicAuthenticationFilter.class)
+            .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
 
-    return http.build();
-
-  }
+        return http.build();
+    }
 }
